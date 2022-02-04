0b996e1a91990439e13d82681c909c7345edda64
Garrett Wong <garrettwong@google.com>
2022-02-04 00:30:33 -0800

    amin

--- main.tf
+++ main.tf
@@ -4,6 +4,45 @@ resource "google_storage_bucket" "static-site" {
   force_destroy = true
 
   uniform_bucket_level_access = true
+}
 
 
+terraform {
+    required_version = "~> 1.0.7"
+    required_providers {
+        google-beta = {
+            source = "hashicorp/google-beta"
+            version = "~> 3.85.0"
+        }
+    }
+}
+
+provider "google-beta" {
+    region      = "europe-west1"
+}
+
+   
+variable "project_id" {
+  type = string
+  description = "The Google Project ID"
+}
+resource "google_iam_workload_identity_pool" "gh_pool" {
+  project                   = var.project_id
+  provider                  = google-beta
+  workload_identity_pool_id = "gh-pool"
+}
+
+resource "google_iam_workload_identity_pool_provider" "provider" {
+  provider                           = google-beta
+  project                            = var.project_id
+  workload_identity_pool_id          = google_iam_workload_identity_pool.gh_pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = "gh-provider"
+  attribute_mapping                  = {
+    "google.subject" = "assertion.sub"
+    "attribute.full" = "assertion.repository+assertion.ref"
+  }
+  oidc {
+    allowed_audiences = ["google-wlif"]
+    issuer_uri        = "https://token.actions.githubusercontent.com"
+  }
 }
