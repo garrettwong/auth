57aa33090439a5ffb501549ca1cd892b823c4694
Garrett Wong <garrettwong@google.com>
2022-02-04 10:24:11 -0800

    main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
-resource "google_storage_bucket" "static-site" {
-  name          = "garrettwong-store"
-  location      = "EU"
-  force_destroy = true
-
-  uniform_bucket_level_access = true
+resource "google_compute_disk" "default" {
+  name  = "test-disk"
+  type  = "pd-ssd"
+  zone  = "us-central1-a"
+  image = "debian-9-stretch-v20200805"
+  labels = {
+    environment = "dev"
+  }
+  physical_block_size_bytes = 4096
 }
 
 
@@ -24,23 +27,23 @@ variable "project_id" {
   type        = string
   description = "The Google Project ID"
 }
-resource "google_iam_workload_identity_pool" "gh_pool" {
-  project                   = var.project_id
-  provider                  = google-beta
-  workload_identity_pool_id = "gh-pool"
-}
+# resource "google_iam_workload_identity_pool" "gh_pool" {
+#   project                   = var.project_id
+#   provider                  = google-beta
+#   workload_identity_pool_id = "gh-pool"
+# }
 
-resource "google_iam_workload_identity_pool_provider" "provider" {
-  provider                           = google-beta
-  project                            = var.project_id
-  workload_identity_pool_id          = google_iam_workload_identity_pool.gh_pool.workload_identity_pool_id
-  workload_identity_pool_provider_id = "gh-provider"
-  attribute_mapping = {
-    "google.subject" = "assertion.sub"
-    "attribute.full" = "assertion.repository+assertion.ref"
-  }
-  oidc {
-    allowed_audiences = ["google-wlif"]
-    issuer_uri        = "https://token.actions.githubusercontent.com"
-  }
-}
+# resource "google_iam_workload_identity_pool_provider" "provider" {
+#   provider                           = google-beta
+#   project                            = var.project_id
+#   workload_identity_pool_id          = google_iam_workload_identity_pool.gh_pool.workload_identity_pool_id
+#   workload_identity_pool_provider_id = "gh-provider"
+#   attribute_mapping = {
+#     "google.subject" = "assertion.sub"
+#     "attribute.full" = "assertion.repository+assertion.ref"
+#   }
+#   oidc {
+#     allowed_audiences = ["google-wlif"]
+#     issuer_uri        = "https://token.actions.githubusercontent.com"
+#   }
+# }
