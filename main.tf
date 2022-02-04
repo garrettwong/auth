18dcc5c5a70790e3388f9a46e0d8ee55224fdeda
Garrett Wong <garrettwong@google.com>
2022-02-04 00:31:43 -0800

    amin

--- main.tf
+++ main.tf
@@ -8,17 +8,15 @@ resource "google_storage_bucket" "static-site" {
 
 
 terraform {
-    required_version = "~> 1.0.7"
     required_providers {
         google-beta = {
             source = "hashicorp/google-beta"
-            version = "~> 3.85.0"
         }
     }
 }
 
 provider "google-beta" {
-    region      = "europe-west1"
+    region      = "us-west1"
 }
 
    
