7582330f7f89e18eab383db62ffd2762082f269d
Garrett Wong <garrettwong@google.com>
2022-02-04 18:33:50 -0800

    fix fmt

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ provider "google-beta" {
 variable "project_id" {
   type        = string
   description = "The Google Project ID"
-  default = "devops-6a24"
+  default     = "devops-6a24"
 }
 
 # resource "google_iam_workload_identity_pool" "gh_pool" {
