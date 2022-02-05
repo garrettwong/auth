76ff86dc04462f90de97c26dbe5283c8148caba7
Garrett Wong <garrettwong@google.com>
2022-02-04 19:06:41 -0800

    nf

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ resource "google_compute_disk" "default" {
   name  = "test-disk"
   type  = "pd-ssd"
   zone  = "us-central1-a"
-  image = "debian-11-bullseye-v20220120"
   labels = {
     environment = "dev"
   }
