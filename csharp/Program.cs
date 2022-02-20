2f8ade7c252beaf0648e97c436c1e3a870560cd1
Garrett Wong <garrettwong@google.com>
2022-02-20 00:35:25 -0800

    add csharp

---
+++ csharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Google.Apis.Storage.v1.Data;
+using Google.Cloud.Storage.V1;
+
+namespace csharp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    //   CreateBucket("gwc-wif", "helliofdnsaklfa");
+        ListBucketContents("helliofdnsaklfa");
+    }
+
+    public static Bucket
+    CreateBucket(
+      string projectId = "gwc-wif",
+      string bucketName = "your-unique-bucket-name123"
+    )
+    {
+      var storage = StorageClient.Create();
+      var bucket = storage.CreateBucket(projectId, bucketName);
+      Console.WriteLine($"Created {bucketName}.");
+      return bucket;
+    }
+
+    public static void ListBucketContents(string bucketName) {
+        var s = StorageClient.Create();
+        var x = s.ListObjects(bucketName);
+        foreach(var a in x) {
+            Console.WriteLine(a.Name);
+        }
+    }
+  }
+}
